84148d0781f9b32dbf4a8ab3f0447da4928c2501
Endeyr <aaron.thompson42594@gmail.com>
2023-02-22 08:31:42 -0500

    added recoil global state for auth buttons

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,44 @@
+import { authModalState } from '@/atoms/authModalAtom'
+import {
+	Button,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+} from '@chakra-ui/react'
+import React from 'react'
+import { useRecoilState } from 'recoil'
+
+const AuthModal: React.FC = () => {
+	const [modalState, setModalState] = useRecoilState(authModalState)
+
+	const handleClose = () => {
+		setModalState((prev) => ({
+			...prev,
+			open: false,
+		}))
+	}
+	return (
+		<>
+			<Modal isOpen={modalState.open} onClose={handleClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>Modal Title</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>Here is the Modal Body</ModalBody>
+
+					<ModalFooter>
+						<Button colorScheme="blue" mr={3} onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant="ghost">Secondary Action</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	)
+}
+export default AuthModal
